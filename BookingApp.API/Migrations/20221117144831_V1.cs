1ec52a07c5fa3536b0c713e38db067fefc84fcd1
duclatao0k <duccccnguyen@gmail.com>
2022-11-17 21:52:06 +0700

    my commit

---
+++ BookingApp.API/Migrations/20221117144831_V1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingApp.API.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
